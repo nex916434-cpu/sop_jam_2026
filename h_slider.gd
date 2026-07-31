94f156d0dbefe312cbec7c4d880129f3bceb8394
nex916434-cpu <nex916434@gmail.com>
2026-07-31 16:26:01 +0400

    4

--- h_slider.gd
+++ h_slider.gd
@@ -1,10 +1,11 @@
 extends HSlider
 
-func _on_h_slider_value_changed(value: float) -> void:
-	AudioServer.set_bus_volume_db(
-		AudioServer.get_bus_index("Master"),
-		linear_to_db(value)
-	)
+
+
 
 func _on_value_changed(value: float) -> void:
-	pass # Replace with function body.
+	var reversed_value = 100.0 - value
+	AudioServer.set_bus_volume_db(
+		AudioServer.get_bus_index("Master"),
+		linear_to_db(reversed_value / 100.0)
+	)
