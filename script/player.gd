1be09c265c4fa6dbc91d2e36eb5a25e4139a6075
nex916434-cpu <nex916434@gmail.com>
2026-07-31 16:38:00 +0400

    4

--- script/player.gd
+++ script/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 @export var speed: float = 300.0
-@export var jump_velocity: float = -500
+@export var jump_velocity: float = -700
 
 var gravity: float = ProjectSettings.get_setting("physics/2d/default_gravity")
 
